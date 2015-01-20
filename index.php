8cd79ac47fabdb6b1593dcc97168fe6922753deb
s2834244 <cbrewin93@gmail.com>
2015-01-20 05:12:00 +0000

    my second commit

--- index.php
+++ index.php
@@ -7,6 +7,44 @@
       
     <body>
       <h1>Hello World</h1>
+      <h2>Chris Brewin</h2>
+      <ol>
+        <li>Chicken</li>
+        <li>Beef</li>
+        <li>Apples</li>
+        <li>grapes></li>
+      </ol>
+      <table>
+        <thead>
+          <tr>
+            <td>Type</td>
+            <td>Name</td>
+            <td>Description</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Frog</td>
+            <td>Fred</td>
+            <td>Its A Frog.</td>
+          <tr>
+          <tr>
+            <td>Cow</td>
+            <td>Lewis</td>
+            <td>Its A cow.</td>
+          <tr>
+          <tr>
+            <td>Chicken</td>
+            <td>Chuck</td>
+            <td>Its A Chicken</td>
+          <tr>
+          <tr>
+            <td>Stick Insect</td>
+            <td>Sticky</td>
+            <td>Its A Strick Insect.</td>
+          <tr>
+        </tbody>
+      </table>
     </body>
-    
+   
   </html>
